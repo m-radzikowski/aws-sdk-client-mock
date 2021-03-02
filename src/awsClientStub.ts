668b7976ecfc7b46bf8af234b17a4f0254677c92
Maciej Radzikowski <maciej@radzikowski.com.pl>
2021-03-03 00:33:33 +0100

    docs: update Client stub and behavior docs

--- src/awsClientStub.ts
+++ src/awsClientStub.ts
@@ -2,16 +2,38 @@
 import {Client, Command, MetadataBearer} from '@aws-sdk/types';
 import {match, SinonSpyCall, SinonStub} from 'sinon';
 
+interface Behavior<TInput extends object, TOutput extends MetadataBearer, TCommandOutput extends TOutput> {
+
+    resolves(response: CommandResponse<TCommandOutput>): AwsStub<TInput, TOutput>;
+
+    rejects(error?: string | Error | AwsError): AwsStub<TInput, TOutput>;
+
+    callsFake(fn: (input: any) => any): AwsStub<TInput, TOutput>; // TODO Types
+
+}
+
+/**
+ * Type for {@link AwsStub} class,
+ * but with the AWS Client class type as an only generic parameter.
+ *
+ * Usage:
+ * ```typescript
+ * let snsMock: AwsClientStub<SNSClient>;
+ * snsMock = mockClient(SNSClient);
+ * ```
+ */
+export type AwsClientStub<TClient extends Client<any, any, any>> =
+    TClient extends Client<infer TInput, infer TOutput, any> ? AwsStub<TInput, TOutput> : never;
+
 /**
  * Wrapper on the mocked `Client#send()` method,
  * allowing to configure its behavior.
  *
  * Without any configuration, `Client#send()` invocation returns `undefined`.
+ *
+ * To define resulting variable type easily, use {@link AwsClientStub}.
  */
-export type AwsClientStub<TClient extends Client<any, any, any>> = TClient extends Client<infer TInput, infer TOutput, any> ? AwsStub<TInput, TOutput> : never;
-
-/** See {@link AwsClientStub} description. */
-export class AwsStub<TInput extends object, TOutput extends MetadataBearer> {
+export class AwsStub<TInput extends object, TOutput extends MetadataBearer> implements Behavior<TInput, TOutput, TOutput> {
 
     /**
      * Underlying `Client#send()` method Sinon stub.
@@ -27,33 +49,25 @@ export class AwsStub<TInput extends object, TOutput extends MetadataBearer> {
         this.anyCommandBehavior = new CommandBehavior(this, send);
     }
 
-    /**
-     * Resets stub's history and behavior.
-     */
+    /** Resets stub's history and behavior. */
     reset(): AwsStub<TInput, TOutput> {
         this.send.reset();
         return this;
     }
 
-    /**
-     * Resets stub's behavior.
-     */
+    /** Resets stub's behavior. */
     resetBehavior(): AwsStub<TInput, TOutput> {
         this.send.resetBehavior();
         return this;
     }
 
-    /**
-     * Resets stub's calls history.
-     */
+    /** Resets stub's calls history. */
     resetHistory(): AwsStub<TInput, TOutput> {
         this.send.resetHistory();
         return this;
     }
 
-    /**
-     * Replaces stub with original `Client#send()` method.
-     */
+    /** Replaces stub with original `Client#send()` method. */
     restore(): void {
         this.send.restore();
     }
@@ -61,7 +75,6 @@ export class AwsStub<TInput extends object, TOutput extends MetadataBearer> {
     /**
      * Returns recorded calls to the stub.
      * Clear history with {@link resetHistory} or {@link reset}.
-     * @return Array of calls
      */
     calls(): SinonSpyCall<[AwsCommand<TInput, TOutput>], unknown>[] {
         return this.send.getCalls();
@@ -69,32 +82,32 @@ export class AwsStub<TInput extends object, TOutput extends MetadataBearer> {
 
     /**
      * Returns n-th recorded call to the stub.
-     * @return Call
      */
     call(n: number): SinonSpyCall<[AwsCommand<TInput, TOutput>], unknown> {
         return this.send.getCall(n);
     }
 
     /**
-     * Specifies a Command type and its input (parameters) for which the next defined `Client#send()` response
-     * (with {@link resolves} or {@link rejects}) will act.
+     * Allows specifying the behavior for a given Command type and its input (parameters).
      *
-     * If payload is not specified, it will match any Command of this type.
+     * If the input is not specified, it will match any Command of that type.
      * @param command Command type to match
      * @param input Command payload to (strictly) match
      */
-    on<TCmdInput extends TInput, TCmdOutput extends TOutput>(command: new (input: TCmdInput) => AwsCommand<TCmdInput, TCmdOutput>, input?: TCmdInput): CommandBehavior<TInput, TOutput, TCmdOutput> {
+    on<TCmdInput extends TInput, TCmdOutput extends TOutput>(
+        command: new (input: TCmdInput) => AwsCommand<TCmdInput, TCmdOutput>, input?: TCmdInput,
+    ): CommandBehavior<TInput, TOutput, TCmdOutput> {
         const matcher = match.instanceOf(command).and(this.createInputMatcher(input));
         const cmdStub = this.send.withArgs(matcher);
         return new CommandBehavior<TInput, TOutput, TCmdOutput>(this, cmdStub);
     }
 
     /**
-     * Specifies a Command input (parameters) for which the next defined `Client#send()` response
-     * (with {@link resolves} or {@link rejects}) will act.
+     * Allows specifying the behavior for any Command with given input (parameters).
      *
-     * If input is not specified, the next defined `Client#send()` response will act for any Command and any input.
-     * This is only for readability, as the result is the same as with not calling it at all.
+     * If the input is not specified, the given behavior will be used for any Command with any input.
+     * This is no different from using {@link resolves}, {@link rejects}, etc. directly,
+     * but can be used for readability.
      * @param input Command payload to (strictly) match
      */
     onAnyCommand<TCmdInput extends TInput>(input?: TCmdInput): CommandBehavior<TInput, TOutput, TOutput> {
@@ -106,21 +119,34 @@ export class AwsStub<TInput extends object, TOutput extends MetadataBearer> {
         return input !== undefined ? match.has('input', input) : match.any;
     }
 
+    /**
+     * Sets a successful response that will be returned from any `Client#send()` invocation.
+     * @param response Content to be returned
+     */
     resolves(response: CommandResponse<TOutput>): AwsStub<TInput, TOutput> {
         return this.anyCommandBehavior.resolves(response);
     }
 
+    /**
+     * Sets a failure response that will be returned from any `Client#send()` invocation.
+     * The response will always be an `Error` instance.
+     * @param error Error text, Error instance or Error parameters to be returned
+     */
     rejects(error?: string | Error | AwsError): AwsStub<TInput, TOutput> {
         return this.anyCommandBehavior.rejects(error);
     }
 
+    /**
+     * Sets a function that will be called on any `Client#send()` invocation.
+     * @param fn Function taking Command input and returning result
+     */
     callsFake(fn: (input: any) => any): AwsStub<TInput, TOutput> {
         return this.anyCommandBehavior.callsFake(fn);
     }
 
 }
 
-export class CommandBehavior<TInput extends object, TOutput extends MetadataBearer, TCommandOutput extends TOutput> {
+export class CommandBehavior<TInput extends object, TOutput extends MetadataBearer, TCommandOutput extends TOutput> implements Behavior<TInput, TOutput, TCommandOutput> {
 
     constructor(
         private clientStub: AwsStub<TInput, TOutput>,
@@ -129,11 +155,9 @@ export class CommandBehavior<TInput extends object, TOutput extends MetadataBear
     }
 
     /**
-     * Sets a successful response that will be returned from the `Client#send()` invocation.
-     *
-     * If a Command and/or its input were specified (with {@link on} or {@link onAnyCommand}),
-     * the response will be returned only on receiving them as `Client#send()` argument.
-     * @param response Content to be returned from `Client#send()` method
+     * Sets a successful response that will be returned from the `Client#send()` invocation
+     * for specified Command and/or its input.
+     * @param response Content to be returned
      */
     resolves(response: CommandResponse<TCommandOutput>): AwsStub<TInput, TOutput> {
         this.send.resolves(response);
@@ -141,11 +165,10 @@ export class CommandBehavior<TInput extends object, TOutput extends MetadataBear
     }
 
     /**
-     * Sets a failure response that will be returned from the `Client#send()` invocation.
+     * Sets a failure response that will be returned from the `Client#send()` invocation
+     * for specified Command and/or its input.
      * The response will always be an `Error` instance.
-     *
-     * The same Command and its input rules apply as in the {@link resolves}.
-     * @param error Error text, Error instance or Error parameters to be returned from `Client#send()` method
+     * @param error Error text, Error instance or Error parameters to be returned
      */
     rejects(error?: string | Error | AwsError): AwsStub<TInput, TOutput> {
         if (typeof error === 'string') {
@@ -161,9 +184,8 @@ export class CommandBehavior<TInput extends object, TOutput extends MetadataBear
     }
 
     /**
-     * Sets a function that will be called on `Client#send()` invocation.
-     *
-     * The same Command and its input rules apply as in the {@link resolves}.
+     * Sets a function that will be called on `Client#send()` invocation
+     * for specified Command and/or its input.
      * @param fn Function taking Command input and returning result
      */
     callsFake(fn: (input: any) => any): AwsStub<TInput, TOutput> {
