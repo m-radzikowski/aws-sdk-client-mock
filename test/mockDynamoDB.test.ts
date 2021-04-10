8271c731579a0fbe33051e1d1d1eb6cc6047e516
Maciej Radzikowski <maciej@radzikowski.com.pl>
2021-04-11 01:21:42 +0200

    test: mocking paginated operation

--- test/mockDynamoDB.test.ts
+++ test/mockDynamoDB.test.ts
@@ -1,15 +1,15 @@
 import {mockClient} from '../src';
-import {DynamoDB, QueryCommand} from '@aws-sdk/client-dynamodb';
+import {DynamoDBClient, paginateQuery, QueryCommand} from '@aws-sdk/client-dynamodb';
 import {DynamoDBDocumentClient, QueryCommand as DocumentQueryCommand} from '@aws-sdk/lib-dynamodb';
 import {marshall} from '@aws-sdk/util-dynamodb';
 
 it('mocks classic DynamoDB client', async () => {
-    const mock = mockClient(DynamoDB);
+    const mock = mockClient(DynamoDBClient);
     mock.on(QueryCommand).resolves({
         Items: [marshall({pk: 'a', sk: 'b'})],
     });
 
-    const dynamodb = new DynamoDB({});
+    const dynamodb = new DynamoDBClient({});
     const query = await dynamodb.send(new QueryCommand({
         TableName: 'mock',
     }));
@@ -24,7 +24,7 @@ it('mocks DynamoDB DocumentClient', async () => {
         Items: [{pk: 'a', sk: 'b'}],
     });
 
-    const dynamodb = new DynamoDB({});
+    const dynamodb = new DynamoDBClient({});
     const ddb = DynamoDBDocumentClient.from(dynamodb);
 
     const query = await ddb.send(new DocumentQueryCommand({
@@ -33,4 +33,29 @@ it('mocks DynamoDB DocumentClient', async () => {
 
     expect(query.Items).toHaveLength(1);
     expect(query.Items?.[0]).toStrictEqual({pk: 'a', sk: 'b'});
+});
+
+it('mocks paginated operation', async () => {
+    const mock = mockClient(DynamoDBClient);
+    mock.on(QueryCommand, {TableName: 'mock', ExclusiveStartKey: undefined, Limit: 1}).resolves({
+        Items: [
+            marshall({pk: 'a', sk: 'b'}),
+        ],
+        LastEvaluatedKey: marshall({pk: 'a', sk: 'b'}),
+    }).on(QueryCommand, {TableName: 'mock', ExclusiveStartKey: marshall({pk: 'a', sk: 'b'}), Limit: 1}).resolves({
+        Items: [
+            marshall({pk: 'c', sk: 'd'}),
+        ],
+    });
+
+    const dynamodb = new DynamoDBClient({});
+    const paginator = paginateQuery({client: dynamodb, pageSize: 1}, {TableName: 'mock'});
+
+    const items = [];
+    for await (const page of paginator) {
+        items.push(...page.Items || []);
+    }
+
+    expect(items).toHaveLength(2);
+    expect(items?.[1]).toStrictEqual({pk: {S: 'c'}, sk: {S: 'd'}});
 });
