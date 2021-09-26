06604270aef93f21052ff161377f21142dc6496b
Maciej Radzikowski <maciej@radzikowski.com.pl>
2021-09-26 16:35:51 +0200

    test: replace compatibility check script with an e2e test (#54)
    * test: replace compatibility check script with an e2e test
    
    Compatibility script was using the library sources,
    and because of that depended on the @aws-sdk/types version installed on the root project.
    That was in conflict with the AWS SDK client versions installed by the compatibility script.
    
    E2E tests deploy package to a local npm registry (Verdaccio),
    install the package from there and run unit tests with it.
    
    * fix: ignore e2e tests from eslint
    
    * fix: ignore verdaccio storage and e2e tests dir in unit tests

---
+++ test-e2e/run-e2e-test.ts
@@ -0,0 +1,15 @@
+import {execSync} from 'child_process';
+import * as packageJson from '../package.json';
+
+const libVersion = packageJson.version;
+
+const exec = (cmd: string) => execSync(cmd, {
+    cwd: __dirname,
+    stdio: 'inherit',
+});
+
+// use npm because when running this script from yarn and using yarn
+// it did not accept private registry parameter set in other way than in a project-level .yarnrc file
+exec('npm init -y');
+exec(`npm install --registry=http://localhost:4873 aws-sdk-client-mock@${libVersion} @aws-sdk/client-sns@latest jest typescript ts-jest`);
+exec('npx jest');
