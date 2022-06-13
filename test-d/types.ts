ea57ca81f3485e96c5df2ed6a324445db498a77e
Jurij Zahn <jurij.zahn@otto.de>
2022-06-13 16:34:57 +0200

    fix: test types

--- test-d/types.ts
+++ test-d/types.ts
@@ -36,10 +36,10 @@ expect(mockClient(SNSClient)).toHaveReceivedCommand(PublishCommand)
 expectError(expect(mockClient(SNSClient)).toHaveReceivedCommand(String))
 
 expect(mockClient(SNSClient)).toHaveReceivedCommandTimes(PublishCommand, 1)
-expectError(expect(mockClient(SNSClient)).toHaveReceivedCommandTimes(PublishCommand, 1))
+expectError(expect(mockClient(SNSClient)).toHaveReceivedCommandTimes(PublishCommand))
 
 expect(mockClient(SNSClient)).toHaveReceivedCommandWith(PublishCommand, {Message: ''})
 expectError(expect(mockClient(SNSClient)).toHaveReceivedCommandWith(PublishCommand, { Foo: '' }))
 
-expect(mockClient(SNSClient)).toHaveNthReceivedCommandWith(1, PublishCommand, {Message: ''})
-expectError(expect(mockClient(SNSClient)).toHaveNthReceivedCommandWith(1, PublishCommand, { Foo: '' }))
+expect(mockClient(SNSClient)).toHaveReceivedNthCommandWith(1, PublishCommand, {Message: ''})
+expectError(expect(mockClient(SNSClient)).toHaveReceivedNthCommandWith(1, PublishCommand, { Foo: '' }))
