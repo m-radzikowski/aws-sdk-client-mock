86c00d648b1b91c12d46c60a031dd6cf593104b4
Maciej Radzikowski <maciej@radzikowski.com.pl>
2023-03-11 20:06:56 +0100

    chore: update Jest
    Jest now uses a separate library for serializing snapshots.
    See: https://github.com/facebook/jest/pull/13040

--- packages/aws-sdk-client-mock-jest/test/jestMatchers.test.ts
+++ packages/aws-sdk-client-mock-jest/test/jestMatchers.test.ts
@@ -23,8 +23,8 @@ describe('toHaveReceivedCommand', () => {
 
     it('fails on not receiving Command', () => {
         expect(() => expect(snsMock).toHaveReceivedCommand(PublishCommand)).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to receive <green>\\"PublishCommand\\"</>
-SNSClient received <green>\\"PublishCommand\\"</> <red>0</> times"
+"Expected SNSClient to receive <green>"PublishCommand"</color>
+SNSClient received <green>"PublishCommand"</color> <red>0</color> times"
 `);
     });
 
@@ -33,11 +33,11 @@ SNSClient received <green>\\"PublishCommand\\"</> <red>0</> times"
         await sns.send(publishCmd1);
 
         expect(() => expect(snsMock).not.toHaveReceivedCommand(PublishCommand)).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to not receive <green>\\"PublishCommand\\"</>
-SNSClient received <green>\\"PublishCommand\\"</> <red>1</> times
+"Expected SNSClient to not receive <green>"PublishCommand"</color>
+SNSClient received <green>"PublishCommand"</color> <red>1</color> times
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 });
@@ -56,11 +56,11 @@ describe('toHaveReceivedCommandTimes', () => {
         await sns.send(publishCmd1);
 
         expect(() => expect(snsMock).toHaveReceivedCommandTimes(PublishCommand, 2)).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to receive <green>\\"PublishCommand\\"</> <green>2</> times
-SNSClient received <green>\\"PublishCommand\\"</> <red>1</> times
+"Expected SNSClient to receive <green>"PublishCommand"</color> <green>2</color> times
+SNSClient received <green>"PublishCommand"</color> <red>1</color> times
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 
@@ -70,12 +70,12 @@ Calls:
         await sns.send(publishCmd1);
 
         expect(() => expect(snsMock).not.toHaveReceivedCommandTimes(PublishCommand, 2)).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to not receive <green>\\"PublishCommand\\"</> <green>2</> times
-SNSClient received <green>\\"PublishCommand\\"</> <red>2</> times
+"Expected SNSClient to not receive <green>"PublishCommand"</color> <green>2</color> times
+SNSClient received <green>"PublishCommand"</color> <red>2</color> times
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>
-  2. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>
+  2. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 });
@@ -94,11 +94,11 @@ describe('toHaveReceivedCommandWith', () => {
         await sns.send(publishCmd1);
 
         expect(() => expect(snsMock).toHaveReceivedCommandWith(PublishCommand, {Message: publishCmd2.input.Message})).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to receive <green>\\"PublishCommand\\"</> with <green>{\\"Message\\": \\"second mock message\\"}</>
-SNSClient received matching <green>\\"PublishCommand\\"</> <red>0</> times
+"Expected SNSClient to receive <green>"PublishCommand"</color> with <green>{"Message": "second mock message"}</color>
+SNSClient received matching <green>"PublishCommand"</color> <red>0</color> times
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 
@@ -107,11 +107,11 @@ Calls:
         await sns.send(publishCmd1);
 
         expect(() => expect(snsMock).not.toHaveReceivedCommandWith(PublishCommand, {Message: publishCmd1.input.Message})).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to not receive <green>\\"PublishCommand\\"</> with <green>{\\"Message\\": \\"mock message\\"}</>
-SNSClient received matching <green>\\"PublishCommand\\"</> <red>1</> times
+"Expected SNSClient to not receive <green>"PublishCommand"</color> with <green>{"Message": "mock message"}</color>
+SNSClient received matching <green>"PublishCommand"</color> <red>1</color> times
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 
@@ -131,11 +131,11 @@ Calls:
         expect(() => expect(snsMock).toHaveReceivedCommandWith(PublishCommand, {
             Message: expect.stringMatching(/qq/), // eslint-disable-line @typescript-eslint/no-unsafe-assignment
         })).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to receive <green>\\"PublishCommand\\"</> with <green>{\\"Message\\": StringMatching /qq/}</>
-SNSClient received matching <green>\\"PublishCommand\\"</> <red>0</> times
+"Expected SNSClient to receive <green>"PublishCommand"</color> with <green>{"Message": StringMatching /qq/}</color>
+SNSClient received matching <green>"PublishCommand"</color> <red>0</color> times
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 });
@@ -155,20 +155,20 @@ describe('toHaveReceivedNthCommandWith', () => {
         await sns.send(publishCmd1);
 
         expect(() => expect(snsMock).toHaveReceivedNthCommandWith(2, PublishCommand, {Message: publishCmd2.input.Message})).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to receive 2. <green>\\"PublishCommand\\"</> with <green>{\\"Message\\": \\"second mock message\\"}</>
-SNSClient received <red>\\"PublishCommand\\"</> with input:
-<green>- Expected  - 1</>
-<red>+ Received  + 2</>
+"Expected SNSClient to receive 2. <green>"PublishCommand"</color> with <green>{"Message": "second mock message"}</color>
+SNSClient received <red>"PublishCommand"</color> with input:
+<green>- Expected  - 1</color>
+<red>+ Received  + 2</color>
 
-<dim>  Object {</>
-<green>-   \\"Message\\": \\"second mock message\\",</>
-<red>+   \\"Message\\": \\"mock message\\",</>
-<red>+   \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\",</>
-<dim>  }</>
+<dim>  Object {</intensity>
+<green>-   "Message": "second mock message",</color>
+<red>+   "Message": "mock message",</color>
+<red>+   "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic",</color>
+<dim>  }</intensity>
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>
-  2. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>
+  2. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 
@@ -178,19 +178,19 @@ Calls:
         await sns.send(publishCmd2);
 
         expect(() => expect(snsMock).not.toHaveReceivedNthCommandWith(2, PublishCommand, {Message: publishCmd2.input.Message})).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to not receive 2. <green>\\"PublishCommand\\"</> with <green>{\\"Message\\": \\"second mock message\\"}</>
-SNSClient received <red>\\"PublishCommand\\"</> with input:
-<green>- Expected  - 0</>
-<red>+ Received  + 1</>
+"Expected SNSClient to not receive 2. <green>"PublishCommand"</color> with <green>{"Message": "second mock message"}</color>
+SNSClient received <red>"PublishCommand"</color> with input:
+<green>- Expected  - 0</color>
+<red>+ Received  + 1</color>
 
-<dim>  Object {</>
-<dim>    \\"Message\\": \\"second mock message\\",</>
-<red>+   \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\",</>
-<dim>  }</>
+<dim>  Object {</intensity>
+<dim>    "Message": "second mock message",</intensity>
+<red>+   "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic",</color>
+<dim>  }</intensity>
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>
-  2. PublishCommand: <red>{\\"Message\\": \\"second mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>
+  2. PublishCommand: <red>{"Message": "second mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 
@@ -199,10 +199,10 @@ Calls:
         await sns.send(publishCmd1);
 
         expect(() => expect(snsMock).toHaveReceivedNthCommandWith(2, PublishCommand, {Message: publishCmd2.input.Message})).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to receive 2. <green>\\"PublishCommand\\"</> with <green>{\\"Message\\": \\"second mock message\\"}</>
+"Expected SNSClient to receive 2. <green>"PublishCommand"</color> with <green>{"Message": "second mock message"}</color>
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 
@@ -224,20 +224,20 @@ Calls:
         expect(() => expect(snsMock).toHaveReceivedNthCommandWith(2, PublishCommand, {
             Message: expect.stringMatching(/qq/), // eslint-disable-line @typescript-eslint/no-unsafe-assignment
         })).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to receive 2. <green>\\"PublishCommand\\"</> with <green>{\\"Message\\": StringMatching /qq/}</>
-SNSClient received <red>\\"PublishCommand\\"</> with input:
-<green>- Expected  - 1</>
-<red>+ Received  + 2</>
+"Expected SNSClient to receive 2. <green>"PublishCommand"</color> with <green>{"Message": StringMatching /qq/}</color>
+SNSClient received <red>"PublishCommand"</color> with input:
+<green>- Expected  - 1</color>
+<red>+ Received  + 2</color>
 
-<dim>  Object {</>
-<green>-   \\"Message\\": StringMatching /qq/,</>
-<red>+   \\"Message\\": \\"second mock message\\",</>
-<red>+   \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\",</>
-<dim>  }</>
+<dim>  Object {</intensity>
+<green>-   "Message": StringMatching /qq/,</color>
+<red>+   "Message": "second mock message",</color>
+<red>+   "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic",</color>
+<dim>  }</intensity>
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>
-  2. PublishCommand: <red>{\\"Message\\": \\"second mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>
+  2. PublishCommand: <red>{"Message": "second mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 });
@@ -257,10 +257,10 @@ describe('toHaveReceivedNthSpecificCommandWith', () => {
         await sns.send(publishCmd1);
 
         expect(() => expect(snsMock).toHaveReceivedNthSpecificCommandWith(2, PublishCommand, {Message: publishCmd2.input.Message})).toThrowErrorMatchingInlineSnapshot(`
-"Expected SNSClient to receive 2. <green>\\"PublishCommand\\"</> with <green>{\\"Message\\": \\"second mock message\\"}</>
+"Expected SNSClient to receive 2. <green>"PublishCommand"</color> with <green>{"Message": "second mock message"}</color>
 
 Calls:
-  1. PublishCommand: <red>{\\"Message\\": \\"mock message\\", \\"TopicArn\\": \\"arn:aws:sns:us-east-1:111111111111:MyTopic\\"}</>"
+  1. PublishCommand: <red>{"Message": "mock message", "TopicArn": "arn:aws:sns:us-east-1:111111111111:MyTopic"}</color>"
 `);
     });
 });
