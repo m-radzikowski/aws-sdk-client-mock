31ff180f58a16d61a38d65fc97d35d1af5d9ff5b
Maciej Radzikowski <maciej@radzikowski.com.pl>
2024-01-01 22:26:29 +0100

    feat(core): ability to use custom Sinon Sandbox (#200)

--- packages/aws-sdk-client-mock/src/mockClient.ts
+++ packages/aws-sdk-client-mock/src/mockClient.ts
@@ -1,5 +1,5 @@
 import {Client, Command, MetadataBearer} from '@smithy/types';
-import {SinonStub, stub} from 'sinon';
+import sinon, {SinonSandbox, SinonStub} from 'sinon';
 import {isSinonStub} from './sinon';
 import {AwsClientStub, AwsStub} from './awsClientStub';
 
@@ -7,10 +7,12 @@ import {AwsClientStub, AwsStub} from './awsClientStub';
  * Creates and attaches a stub of the `Client#send()` method. Only this single method is mocked.
  * If method is already a stub, it's replaced.
  * @param client `Client` type or instance to replace the method
+ * @param sandbox Optional sinon sandbox to use
  * @return Stub allowing to configure Client's behavior
  */
 export const mockClient = <TInput extends object, TOutput extends MetadataBearer, TConfiguration>(
     client: InstanceOrClassType<Client<TInput, TOutput, TConfiguration>>,
+    {sandbox}: { sandbox?: SinonSandbox } = {},
 ): AwsClientStub<Client<TInput, TOutput, TConfiguration>> => {
     const instance = isClientInstance(client) ? client : client.prototype;
 
@@ -19,7 +21,8 @@ export const mockClient = <TInput extends object, TOutput extends MetadataBearer
         send.restore();
     }
 
-    const sendStub = stub(instance, 'send') as SinonStub<[Command<TInput, any, TOutput, any, any>], Promise<TOutput>>;
+    const sinonSandbox = sandbox || sinon;
+    const sendStub = sinonSandbox.stub(instance, 'send') as SinonStub<[Command<TInput, any, TOutput, any, any>], Promise<TOutput>>;
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     return new AwsStub<TInput, TOutput, TConfiguration>(instance, sendStub);
